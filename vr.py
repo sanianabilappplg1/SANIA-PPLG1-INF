b2d64e1e05c8f89a3f9c89cc6a67d3896f188151
sanianabilappplg1 <sanianabilaputri83@gmail.com>
2025-05-08 13:44:30 +0700

    VARIABLE

---
+++ vr.py
@@ -0,0 +1,15 @@
+# Materi Variabel 
+
+# Variabel adalah tempat untuk menyimpan data
+
+# Contoh variabel
+nama = "sania"         # Menyimpan data string
+usia = 15              # Menyimpan data integer
+kelas = 10             # Menyimpan data float
+is_siswa = True        # Menyimpan data boolean
+
+# Menampilkan nilai dari variabel
+print("Nama:", nama)
+print("Usia:", usia)
+print("kelas:", kelas)
+print("Apakah sania seorang siswa?:", is_siswa)
