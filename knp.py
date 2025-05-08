6869d15e972e84a2b4f3c648c0b4cd01e366e277
sanianabilappplg1 <sanianabilaputri83@gmail.com>
2025-05-08 13:45:50 +0700

    KONDISI PYHTON

---
+++ knp.py
@@ -0,0 +1,14 @@
+hari = 3
+
+if hari == 1:
+    print("Senin")
+elif hari == 2:
+    print("Selasa")
+elif hari == 3:
+    print("Rabu")
+elif hari == 4:
+    print("Kamis")
+elif hari == 5:
+    print("Jumat")
+else:
+    print("Akhir pekan")
