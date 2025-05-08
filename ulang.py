b22df95aebc341c34f88dc2588799dd8c4642373
sanianabilappplg1 <sanianabilaputri83@gmail.com>
2025-05-08 13:32:31 +0700

    PENGULANGAN

---
+++ ulang.py
@@ -0,0 +1,14 @@
+# WHILE LOOP
+i = 1
+while i <= 2:
+    print(f"Nama saya sania, ini perulangan ke-{i}")
+    i += 1
+
+# FOR LOOP
+for i in range(1, 2):
+    print(f"Nama saya sania, ini perulangan ke-{i}")
+
+# NESTED LOOP
+for i in range(1, 2):
+    for j in range(1, 2):
+        print(f"sania - Baris {i}, Kolom {j}")
