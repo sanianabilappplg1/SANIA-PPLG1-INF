7533d37d9f31e5f71ef5b7e6277b2a31f7fc80ad
sanianabilappplg1 <sanianabilaputri83@gmail.com>
2025-05-08 13:26:04 +0700

    KOMENTAR

---
+++ kom.py
@@ -0,0 +1,14 @@
+# Program materi komentar
+#komentar dengan tanda pagar hanya bisa digunakan
+#untuk
+#satu
+#baris
+
+"""
+Penulisan Komentar lebih dari satu baris yaitu
+dengan menggunakan kutip dua 3 kali dan
+ditutup dengan kutip dua 3 kali juga
+"""
+print("===== Data Diri =====")
+print("sania") # Menampilkan nama pengguna
+print("X PPLG 1") # menampilkan kota pengguna
