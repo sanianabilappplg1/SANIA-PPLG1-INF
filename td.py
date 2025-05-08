175de1905c91fdbe08807d82d6d92cdc0f9c6fe9
sanianabilappplg1 <sanianabilaputri83@gmail.com>
2025-05-08 13:30:47 +0700

    TIPE DATA

---
+++ td.py
@@ -0,0 +1,21 @@
+# materi Tipe Data 
+# Tipe data String (str)
+nama = "Sania nabila putri"
+print(nama)  # Menampilkan nama
+
+# Tipe data Integer (int)
+usia = 15
+print(usia)  # Menampilkan usia
+
+# Tipe data Float (float)
+kelas = 10
+print(kelas)  # Menampilkan kelas
+
+# Tipe data Boolean (bool)
+is_siswa = True
+print(is_siswa)  # Menampilkan True
+
+# Tipe data List (list)
+hobi = ["membaca", "menulis"]
+print(hobi)  # Menampilkan list hobi
+
